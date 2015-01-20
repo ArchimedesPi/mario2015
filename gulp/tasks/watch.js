2846888f07c949c4b6c84493780dbd7587b84447
Liam M <shakespeares.integral@gmail.com>
2015-01-19 22:54:00 -0600

    Change up some tasks

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,5 +3,6 @@ var gulp = require('gulp');
 gulp.task('watch', ['setWatch'], function() {
   gulp.watch('styles/**/*.scss', ['sass']);
   gulp.watch('markup/*.jade', ['markup']);
-  gulp.watch('scripts/*.js', ['browserify'])
+  gulp.watch('scripts/*.js', ['browserify']);
+  gulp.watch('images/*', ['images']);
 });
