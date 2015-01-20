38b115e47d6b18c303b20ec0dca4d170a3c94f9e
Liam M <shakespeares.integral@gmail.com>
2015-01-19 22:56:21 -0600

    Smaller game div

--- scripts/app.js
+++ scripts/app.js
@@ -4,7 +4,7 @@ var Game = {};
 Game.Boot = require('./game/boot');
 Game.Load = require('./game/load');
 
-var game = new Phaser.Game(800, 890, Phaser.AUTO, 'game');
+var game = new Phaser.Game(400, 480, Phaser.AUTO, 'game');
 game.state.add('Boot', Game.Boot);
 game.state.add('Load', Game.Load);
 //game.state.add(Game.Menu);
